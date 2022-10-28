7aed9302408007ad062db20ee4e4930d398f8007
TARIK-MAC <administrateur@mbpdetarikmac.lan>
2022-10-28 16:09:14 +0200

    ajout formulaire et fonction callback sur le DOM

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// récupérer le infos du formulaires
+
+document.addEventListener("DOMContentLoaded", () => {
+  //console.log("loaded");
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", async (event) => {
+      //on mets les valeurs dans un objet
+      event.preventDefault();
+      const data = {
+        firstname: document.getElementById("firstname").value,
+        lastname: document.getElementById("lastname").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+      };
+
+      //console.log(data);
+
+      //envoyer les données avec axios
+      //const result = await axios.post("http://localhost:3000/send-data", data);
+
+      //console.log(result.data);
+    });
+});
